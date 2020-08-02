8b3b44e4ce0a90c1a10205417d45950bd218e384
katerynapidhorna <katerynapidhorna@gmail.com>
2020-08-02 21:45:24 +0200

    Use prod postgres

--- config/config.js
+++ config/config.js
@@ -1,24 +1,21 @@
-require('dotenv').config()
-
+require("dotenv").config();
 
 module.exports = {
   development: {
     url: process.env.DATABASE_URL_DEV,
     dialect: "postgres",
-    operatorsAliases:"0"
+    operatorsAliases: "0",
   },
   test: {
     username: "root",
     password: null,
     database: "database_test",
     host: "127.0.0.1",
-    dialect: "mysql"
+    dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql"
-  }
-}
+    url: process.env.DATABASE_URL,
+    dialect: "postgres",
+    operatorsAliases: "0",
+  },
+};
